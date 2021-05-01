dabe62707361eab6c50d749e66f43f066874df3d
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-05-01 18:40:16 +0100

    generated static readme

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -9,47 +10,47 @@ const questions = [
     message: "What is the title of the project?",
     name: "projectTitle",
   },
-  {
-    type: "input",
-    message: "Please provide a brief description of the project.",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "How do you install your project?",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message: "How do you use your application?",
-    name: "usage",
-  },
-  {
-    type: "list",
-    message: "Who is your project licensed by?",
-    choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
-    name: "license",
-  },
-  {
-    type: "input",
-    message: "How can someone contribute to your project?",
-    name: "contributing",
-  },
-  {
-    type: "input",
-    message: "What tests are there for this project?",
-    name: "testing",
-  },
-  {
-    type: "input",
-    message: "What is your GitHub URL?",
-    name: "githubUrl",
-  },
-  {
-    type: "input",
-    message: "What is your email address?",
-    name: "email",
-  },
+  // {
+  //   type: "input",
+  //   message: "Please provide a brief description of the project.",
+  //   name: "description",
+  // },
+  // {
+  //   type: "input",
+  //   message: "How do you install your project?",
+  //   name: "installation",
+  // },
+  // {
+  //   type: "input",
+  //   message: "How do you use your application?",
+  //   name: "usage",
+  // },
+  // {
+  //   type: "list",
+  //   message: "Who is your project licensed by?",
+  //   choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
+  //   name: "license",
+  // },
+  // {
+  //   type: "input",
+  //   message: "How can someone contribute to your project?",
+  //   name: "contributing",
+  // },
+  // {
+  //   type: "input",
+  //   message: "What tests are there for this project?",
+  //   name: "testing",
+  // },
+  // {
+  //   type: "input",
+  //   message: "What is your GitHub URL?",
+  //   name: "githubUrl",
+  // },
+  // {
+  //   type: "input",
+  //   message: "What is your email address?",
+  //   name: "email",
+  // },
 ];
 
 // TODO: Create a function to write README file
@@ -58,7 +59,8 @@ const writeToFile = (fileName, data) => {};
 // TODO: Create a function to initialize app
 const init = async () => {
   const answers = await inquirer.prompt(questions);
-  console.log(answers);
+  const generatedMarkdown = generateMarkdown(answers);
+  console.log(generatedMarkdown);
 };
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,49 @@ const renderLicenseSection = (license) => {};
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
-  return `# ${data.title}`;
+  return `# Project Title
+
+  ## Description
+  
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque congue commodo ornare. Integer non ligula semper, consequat sapien vel, laoreet.
+  
+  ## Table of Contents
+  
+  [Description](#description)  
+  [Installation](#installation)  
+  [Usage](#usage)  
+  [License](#license)  
+  [Contributing](#contributing)  
+  [Tests](#tests)  
+  [Questions](#questions)
+  
+  ## Installation
+  
+  \`\`\`
+  npm install
+  node index.js
+  \`\`\`
+  
+  ## Usage
+  
+  Instructions for how to use the app.
+  
+  ## License
+  
+  This app is licensed by MIT.
+  
+  ## Contributing
+  
+  Open an issue or pull request.
+  
+  ## Tests
+  
+  There are no tests for this app.
+  
+  ## Questions
+  
+  - Visit my [Github](#)
+  - Email me at: email@email.com`;
 };
 
 module.exports = generateMarkdown;
