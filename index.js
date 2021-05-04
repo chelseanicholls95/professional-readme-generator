dbe6499092a49e77aeaa84bd29b1b35dd9e53e69
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-05-04 22:03:50 +0100

    added when properties to the questions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
+const manageAnswers = require("./utils/manageAnswers");
 
 const questions = [
   {
@@ -8,47 +9,106 @@ const questions = [
     message: "What is the title of the project?",
     name: "projectTitle",
   },
-  {
-    type: "input",
-    message: "Please provide a brief description of the project.",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "How do you install your project?",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message: "How do you use your application?",
-    name: "usage",
-  },
-  {
-    type: "list",
-    message: "Who is your project licensed by?",
-    choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
-    name: "license",
-  },
-  {
-    type: "input",
-    message: "How can someone contribute to your project?",
-    name: "contributing",
-  },
-  {
-    type: "input",
-    message: "What tests are there for this project?",
-    name: "testing",
-  },
-  {
-    type: "input",
-    message: "What is your GitHub URL?",
-    name: "githubUrl",
-  },
-  {
-    type: "input",
-    message: "What is your email address?",
-    name: "email",
-  },
+  // {
+  //   type: "confirm",
+  //   message: "Do you want to add a description?",
+  //   name: "descriptionSection",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Please provide a brief description of the project.",
+  //   name: "descriptionInfo",
+  //   when: (answers) => {
+  //     return answers.descriptionSection;
+  //   },
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Do you want to add an installation section?",
+  //   name: "installationSection",
+  // },
+  // {
+  //   type: "input",
+  //   message: "How do you install your project?",
+  //   name: "installationInfo",
+  //   when: (answers) => {
+  //     return answers.installationSection;
+  //   },
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Do you want to add an usage section?",
+  //   name: "usageSection",
+  // },
+  // {
+  //   type: "input",
+  //   message: "How do you use your application?",
+  //   name: "usageInfo",
+  //   when: (answers) => {
+  //     return answers.usageSection;
+  //   },
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Do you want to add an usage section?",
+  //   name: "licenseSection",
+  // },
+  // {
+  //   type: "list",
+  //   message: "Who is your project licensed by?",
+  //   choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
+  //   name: "licenseInfo",
+  //   when: (answers) => {
+  //     return answers.licenseSection;
+  //   },
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Do you want to add an contributing section?",
+  //   name: "contributingSection",
+  // },
+  // {
+  //   type: "input",
+  //   message: "How can someone contribute to your project?",
+  //   name: "contributingInfo",
+  //   when: (answers) => {
+  //     return answers.contributingSection;
+  //   },
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Do you want to add an testing section?",
+  //   name: "testingSection",
+  // },
+  // {
+  //   type: "input",
+  //   message: "What tests are there for this project?",
+  //   name: "testingInfo",
+  //   when: (answers) => {
+  //     return answers.testingSection;
+  //   },
+  // },
+  // {
+  //   type: "confirm",
+  //   message: "Do you want to add a questions section?",
+  //   name: "questionsSection",
+  // },
+  // {
+  //   type: "input",
+  //   message: "What is your GitHub URL?",
+  //   name: "githubUrl",
+  //   when: (answers) => {
+  //     return answers.questionsSection;
+  //   },
+  // },
+  // {
+  //   type: "input",
+  //   message: "What is your email address?",
+  //   name: "email",
+  //   when: (answers) => {
+  //     return answers.questionsSection;
+  //   },
+  // },
 ];
 
 const writeToFile = (data) => {
@@ -65,8 +125,10 @@ const writeToFile = (data) => {
 
 const init = async () => {
   const answers = await inquirer.prompt(questions);
-  const generatedMarkdown = generateMarkdown(answers);
-  writeToFile(generatedMarkdown);
+  const managedAnswers = manageAnswers(answers);
+  console.log(managedAnswers);
+  // const generatedMarkdown = generateMarkdown(answers);
+  // writeToFile(generatedMarkdown);
 };
 
 // Function call to initialize app
